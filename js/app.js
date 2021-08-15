6e75d77c3fe54071ddc6dc5a9ebc4ea7edf9e9cc
jacobcabreranieto <84207233+jacobcabreranieto@users.noreply.github.com>
2021-08-14 20:10:13 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,42 @@
+//operations functions
+const sum = function (a, b) {
+  return a + b;
+};
+
+const substraction = function (a, b) {
+  return a - b;
+};
+
+const multiplication = function (a, b) {
+  return a * b;
+};
+
+const division = function (a, b) {
+  return a / b;
+};
+
+const power = function (a, b) {
+  return Math.pow(a, b);
+};
+
+const greaterThan = function (a, b) {
+  return a > b;
+};
+
+const minerThan = function (a, b) {
+  return a < b;
+};
+
+const greaterEqualThan = function (a, b) {
+  return a >= b;
+};
+
+const minerEqualThan = function (a, b) {
+  return a <= b;
+};
+
+const equal = function (a, b) {
+  return a === b;
+};
+
+
